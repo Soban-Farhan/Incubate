7a11948da7fb439d085cb13296aa70601194ea2a
Soban Farhan <soban.farhan@dcmail.ca>
2021-01-26 19:56:38 -0500

    Login Template
    Started to design login template. Also, added auth js for future login/registration usage.

--- Incubate/client/src/views/login.js
+++ Incubate/client/src/views/login.js
@@ -3,7 +3,60 @@ import '../css/master.css'
 
 function Login() {
     return (
-      <h2>Login</h2>
+      <div className="container v-center">
+        <div className="card" style={{ boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.15)" }}>
+          <img className="card-img-top img-fluid" src="" alt="Image Banner" />
+          <div className="card-body">
+              <form>
+                  <div className="p-2">
+                  </div>
+                  <div className="row">
+                      <div className="col-lg-2">
+                      </div>
+                      <div className="col-lg-8">
+                          <div className="row">
+                              <div className="col-lg-2 text-left">
+                                  <label> <b> Email </b> </label>
+                              </div>
+                              <div className="">
+                              </div>
+                              <div className="col-lg-10">
+                                  <input />
+                                  <div style={{ color: "red" }}>
+                                      <p>Login Error</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <p className="p-2"></p>
+                          <div className="row">
+                              <div className="col-lg-2">
+                                  <label> <b> Password </b> </label>
+                              </div>
+                              <div className="col-lg-10">
+                                  <input />
+                                  <div style={{ color: "red" }}>
+                                    <p>Password Error</p>
+                                  </div>
+                                  <div style={{ color: "red" }}>
+                                      <p>Other Error</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <p className="p-1"></p>
+                          <div className="row">
+                              <div className="col-lg-10 offset-lg-2 text-right">
+                                  <button type="submit" className="btn btn-outline-dark btn-md"> Login </button>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="col-lg-2">
+                      </div>
+                      <p className="p-1"></p>
+                  </div>
+              </form>
+          </div>
+        </div>
+      </div>
     );
 }
 
