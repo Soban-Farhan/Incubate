5e6aace33563d859b2614ba1e170c7fe7250e041
Soban Farhan <soban.farhan@dcmail.ca>
2021-01-27 21:41:39 -0500

    Bootstrap React
    Added bootstrap to the project and updated login template

--- Incubate/client/src/views/login.js
+++ Incubate/client/src/views/login.js
@@ -1,62 +1,63 @@
 import React from 'react';
-import '../css/master.css'
+import '../css/master.css';
+import banner from '../images/loginBanner.jpg'
+
+// Bootstrap
+import { Container, Col, Row, Card } from 'react-bootstrap';
 
 function Login() {
     return (
-      <div className="container v-center">
-        <div className="card" style={{ boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.15)" }}>
-          <img className="card-img-top img-fluid" src="" alt="Image Banner" />
-          <div className="card-body">
-              <form>
-                  <div className="p-2">
-                  </div>
-                  <div className="row">
-                      <div className="col-lg-2">
-                      </div>
-                      <div className="col-lg-8">
-                          <div className="row">
-                              <div className="col-lg-2 text-left">
-                                  <label> <b> Email </b> </label>
-                              </div>
-                              <div className="">
-                              </div>
-                              <div className="col-lg-10">
-                                  <input />
-                                  <div style={{ color: "red" }}>
-                                      <p>Login Error</p>
-                                  </div>
-                              </div>
-                          </div>
-                          <p className="p-2"></p>
-                          <div className="row">
-                              <div className="col-lg-2">
-                                  <label> <b> Password </b> </label>
-                              </div>
-                              <div className="col-lg-10">
-                                  <input />
-                                  <div style={{ color: "red" }}>
-                                    <p>Password Error</p>
-                                  </div>
-                                  <div style={{ color: "red" }}>
-                                      <p>Other Error</p>
-                                  </div>
-                              </div>
-                          </div>
-                          <p className="p-1"></p>
-                          <div className="row">
-                              <div className="col-lg-10 offset-lg-2 text-right">
-                                  <button type="submit" className="btn btn-outline-dark btn-md"> Login </button>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="col-lg-2">
-                      </div>
-                      <p className="p-1"></p>
-                  </div>
-              </form>
-          </div>
-        </div>
-      </div>
+        <Container className="v-center">
+            <Card style={{ boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.15)" }}>
+                <img className="card-img-top img-fluid" src={banner} alt="Image Banner" />
+                <div className="card-body">
+                    <form>
+                        <div className="p-2">
+                        </div>
+                        <Row>
+                            <Col lg={2}/>
+                            <Col lg={8}>
+                                <p className="p-1"></p>
+                                <Row>       
+                                    <Col lg={2} className="text-left">
+                                        <label className="font-karla"> Email </label>
+                                    </Col>
+                                    <Col lg={10}>
+                                        <input className="input-style font-karla" />
+                                        <div style={{ color: "red" }}>
+                                            <p>Login Error</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <p className="p-2"></p>
+                                <Row>
+                                    <Col lg={2}>
+                                        <label className="font-karla"> Password </label>
+                                    </Col>
+                                    <Col lg={10}>
+                                        <input className="input-style font-karla" />
+                                        <div style={{ color: "red" }}>
+                                        <p>Password Error</p>
+                                        </div>
+                                        <div style={{ color: "red" }}>
+                                            <p>Other Error</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <p className="p-1"></p>
+                                <Row>
+                                    <div className="col-lg-10 offset-lg-2 text-right">
+                                        <button type="submit" className="btn btn-outline-dark btn-md"> Login </button>
+                                    </div>
+                                </Row>
+                            </Col>
+                            <Col lg={2}/>
+                            <p className="p-1"></p>
+                        </Row>
+                    </form>
+                </div>
+            </Card>
+        </Container>
     );
 }
 
