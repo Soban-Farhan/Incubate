9fe3ec57fa95ad0890d32bd9b3c73cbec403da6e
Soban Farhan <soban.farhan@dcmail.ca>
2021-02-23 22:34:50 -0500

    Board development
    starting developing boards. This includes designing layout and putting needed functionality.

--- Incubate/client/src/views/board/index.js
+++ Incubate/client/src/views/board/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
 import '../../css/master.css'
+import logo from '../../images/incuabte.png'
 import { getSessionCookie } from '../../includes/function'
 
-function Board() {
-  console.log(getSessionCookie())
+class Board extends Component {
+    
+  componentDidMount() {
+      if (getSessionCookie() === null) {
+          window.location = "/boards"
+      }
+  }
+
+  render() {
     return (
-      <h2>Boards</h2>
+      <>
+        <div className="container-fluid p-0">
+          <Navbar collapseOnSelect expand="sm" style={{ backgroundColor: "#202020" }} variant="dark">
+            <Navbar.Brand href="#home"><img src={logo} alt="" width={150}/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto" />
+              <Nav>
+                <a href="/logout">
+                  <button className="btn btn-outline-light btn-md font-karla">Log Out</button>
+                </a>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </>
     );
+  }
 }
 
 export default Board;
